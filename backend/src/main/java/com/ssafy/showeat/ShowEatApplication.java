6fd5c103add426c4bcd0cd0174098d5ccbd7f7f2
Haaarimmm <haaarimmm@gmail.com>
2023-10-17 10:12:28 +0900

    Init: Initial commit

---
+++ backend/src/main/java/com/ssafy/showeat/ShowEatApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.showeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowEatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/showeat/ShowEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.showeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
