8eeccf873fe93067d6d9a15c787ffd0cd84a1874
Haaarimmm <haaarimmm@gmail.com>
2023-10-22 23:57:55 +0900

    Feat: Add update coupon state unit test code

--- backend/src/test/java/com/ssafy/showeat/domain/coupon/service/CouponServiceTest.java
+++ backend/src/test/java/com/ssafy/showeat/domain/coupon/service/CouponServiceTest.java
@@ -1,5 +1,6 @@
 package com.ssafy.showeat.domain.coupon.service;
 
+import static com.ssafy.showeat.domain.coupon.entity.CouponState.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.ssafy.showeat.domain.coupon.dto.request.UpdateCouponStateRequestDto;
 import com.ssafy.showeat.domain.coupon.dto.response.CouponResponseDto;
 import com.ssafy.showeat.domain.coupon.entity.Coupon;
 import com.ssafy.showeat.domain.coupon.entity.CouponState;
@@ -34,25 +36,28 @@ public class CouponServiceTest {
 
 	@Test
 	public void testGetCouponListByUserId() {
+		// 1. given
 		// Mock 데이터 생성
 		Long userId = 1L;
 		User user = new User(); // 유저 객체 생성
 		List<Coupon> mockCouponList = new ArrayList<>();
-		Coupon coupon1 = new Coupon(1L, 10, CouponState.ACTIVE, user, new Funding());
-		Coupon coupon2 = new Coupon(2L, 20, CouponState.EXPIRED, user, new Funding());
+		Coupon coupon1 = new Coupon(1L, 10, ACTIVE, user, new Funding());
+		Coupon coupon2 = new Coupon(2L, 20, EXPIRED, user, new Funding());
 		mockCouponList.add(coupon1);
 		mockCouponList.add(coupon2);
 
+		// 2. when
 		// UserRepository findById 메서드 Mock 설정
 		when(userRepository.findById(userId)).thenReturn(Optional.of(user));
 
 		// CouponRepository findByUser 메서드 Mock 설정
 		when(couponRepository.findByUser(user)).thenReturn(mockCouponList);
 
-		// 테스트 대상 메서드 호출
+		// 3. then
+		// getCouponListByUserId 메서드 호출
 		List<CouponResponseDto> couponList = couponService.getCouponListByUserId(userId);
 
-		// 결과 검증
+		// 4. assert
 		assertNotNull(couponList);
 		assertEquals(2, couponList.size());
 
@@ -65,5 +70,28 @@ public class CouponServiceTest {
 		assertEquals(2L, secondCoupon.getCouponId());
 		assertEquals(20, secondCoupon.getCouponPrice());
 		assertEquals(CouponState.EXPIRED, secondCoupon.getCouponState());
+	}
+
+	@Test
+	public void testUpdateCouponState() {
+		// 1. given
+		// Mock 데이터 생성
+		Long couponId = 1L;
+		CouponState newState = USED;
+		Coupon coupon = new Coupon(1L, 10, ACTIVE, new User(), new Funding());
+		UpdateCouponStateRequestDto updateCouponStateRequestDto = new UpdateCouponStateRequestDto();
+		updateCouponStateRequestDto.setCouponId(couponId);
+		updateCouponStateRequestDto.setCouponState(newState);
+
+		// 2. when
+		// CouponRepository findById 메서드 Mock 설정
+		when(couponRepository.findById(couponId)).thenReturn(Optional.of(coupon));
+
+		// 3. then
+		// updateCouponState 메서드 호출
+		couponService.updateCouponState(updateCouponStateRequestDto);
+
+		// 4. assert
+		assertEquals(newState, coupon.getCouponState());
 	}
 }
