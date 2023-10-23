9b46961c45198e9da77380f07d9875d854f3103a
Haaarimmm <haaarimmm@gmail.com>
2023-10-23 17:53:19 +0900

    Fix: Modify user exception code

--- backend/src/main/java/com/ssafy/showeat/global/exception/ExceptionController.java
+++ backend/src/main/java/com/ssafy/showeat/global/exception/ExceptionController.java
@@ -20,11 +20,11 @@ public class ExceptionController {
 		return ResponseResult.exceptionResponse(ExceptionCode.SERVER_EXCEPTION, err.getMessage());
 	}
 
-	@ExceptionHandler(NotExistAccountException.class)
-	public ResponseResult NotExistAccountException(NotExistAccountException err) {
+	@ExceptionHandler(NotExistBusinessException.class)
+	public ResponseResult NotExistBusinessException(NotExistBusinessException err) {
 		log.info("Error : {}", err.getClass());
 		log.info("Error Message : {}", err.getMessage());
-		return ResponseResult.exceptionResponse(ExceptionCode.NOT_EXIST_ACCOUNT_EXCEPTION, err.getMessage());
+		return ResponseResult.exceptionResponse(ExceptionCode.NOT_EXIST_BUSINESS_EXCEPTION, err.getMessage());
 	}
 
 	@ExceptionHandler(NotExistCouponException.class)
