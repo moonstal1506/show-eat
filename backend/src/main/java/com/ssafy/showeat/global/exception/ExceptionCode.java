5d56fd83149c8aebf0db186594423eaf1a8d2730
psg980331@naver.com <psg980331@naver.com>
2023-10-23 15:28:09 +0900

    Feat: Create funding apply exception

--- backend/src/main/java/com/ssafy/showeat/global/exception/ExceptionCode.java
+++ backend/src/main/java/com/ssafy/showeat/global/exception/ExceptionCode.java
@@ -9,6 +9,9 @@ public enum ExceptionCode {
 	NOT_EXIST_ACCOUNT_EXCEPTION(401, "해당 계정이 존재하지 않습니다."),
 	NOT_EXIST_COUPON_EXCEPTION(402, "해당 쿠폰이 존재하지 않습니다."),
 	NOT_EXIST_USER_EXCEPTION(450,"사용자를 찾을 수 없습니다."),
+	NOT_EXIST_FUNDING_EXCEPTION(470,"펀딩을 찾을 수 없습니다."),
+	INACTIVE_FUNDING_EXCEPTION(471,"이미 종료된 펀딩입니다."),
+	IMPOSSIBLE_APPLY_FUNDING_EXCEPTION(472,"펀딩 참여 가능한 자리가 없습니다."),
 	SERVER_EXCEPTION(500, "서버에서 예측하지 못한 에러가 발생했습니다.");
 	private final int errorCode;
 	private final String errorMessage;
