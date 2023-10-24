2794ebfc8a378b0f968e698fca9fcf37cba5352c
Narn99 <vkdldjtmflr@naver.com>
2023-10-24 18:00:50 +0900

    Feat: Add MenuButton
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/components/common/button/MenuButton.tsx
+++ frontend/showeat/src/components/common/button/MenuButton.tsx
@@ -1,5 +1,55 @@
-function MenuButton() {
-    return <div />;
+import styled from "@emotion/styled";
+import Image from "next/image";
+import { ButtonProps } from "@/types/commonProps";
+
+const MenuButtonContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: "center";
+    align-items: center;
+    text-align: center;
+`;
+
+const ImageWrapper = styled("div")<ButtonProps>`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    min-width: 80px;
+    min-height: 80px;
+    position: relative;
+`;
+
+const TextWrapper = styled("span")`
+    padding-top: 10px;
+    font-size: 16px;
+    font-weight: 700;
+`;
+
+function MenuButton({
+    width,
+    height,
+    onClick,
+    // imageURL,
+    menuName,
+}: ButtonProps & {
+    // imageURL: string;
+    menuName: string;
+}) {
+    return (
+        <MenuButtonContainer onClick={onClick}>
+            <ImageWrapper width={width} height={height}>
+                <Image
+                    src="/assets/images/핥짝.jpg"
+                    // src={imageURL}
+                    alt="Menu Button Image"
+                    fill
+                    style={{
+                        borderRadius: "20px",
+                    }}
+                />
+            </ImageWrapper>
+            <TextWrapper>{menuName}</TextWrapper>
+        </MenuButtonContainer>
+    );
 }
 
 export default MenuButton;
--- frontend/showeat/src/pages/index.tsx
+++ frontend/showeat/src/pages/index.tsx
@@ -1,17 +1,32 @@
 /* Import */
 import Head from "next/head";
+import { MenuButton } from "@/components/common/button";
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Home Component */
 function Home() {
+    const handleMenuButton = () => {
+        console.log("hi");
+    };
+
     return (
         <>
             <Head>
                 <title>쑈잇: ShowEat</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <main>구본웅, 그는 신인가!</main>
+            <main>
+                구본웅, 그는 신인가!
+                <MenuButton
+                    width="8vw"
+                    height="8vw"
+                    onClick={handleMenuButton}
+                    // imageURL={}
+                    // menuName={}
+                    menuName="고양이"
+                />
+            </main>
         </>
     );
 }
