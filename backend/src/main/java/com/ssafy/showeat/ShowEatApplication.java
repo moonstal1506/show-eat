e711c602d0bbdeae8487c74ccf6ec93ac7f73900
ooyouj <ooyouj@naver.com>
2023-10-24 15:44:05 +0900

    Fix: Modify ShowEatApplication

--- backend/src/main/java/com/ssafy/showeat/ShowEatApplication.java
+++ backend/src/main/java/com/ssafy/showeat/ShowEatApplication.java
@@ -2,8 +2,14 @@ package com.ssafy.showeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class})
+@EnableJpaAuditing
 public class ShowEatApplication {
 
 	public static void main(String[] args) {
