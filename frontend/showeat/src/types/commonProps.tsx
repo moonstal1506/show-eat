6aedfd5e7d2aea1323003455c3a3af2c52864689
Crassula1994 <gogun3535@gmail.com>
2023-10-24 15:28:56 +0900

    Feat: Add button props type
    Co-authored-by: Narn99 <https://github.com/Narn99>

---
+++ frontend/showeat/src/types/commonProps.tsx
@@ -0,0 +1,11 @@
+/* Import */
+import React from "react";
+
+// ----------------------------------------------------------------------------------------------------
+
+/* Export */
+export interface ButtonProps {
+    width: string;
+    height?: string;
+    onClick: (event: React.MouseEvent<HTMLElement>) => void;
+}
