20e391992c650e234a67605e06285cc562804137
Narn99 <vkdldjtmflr@naver.com>
2023-10-25 15:16:42 +0900

    Feat: Add text input commonprops
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/types/commonProps.tsx
+++ frontend/showeat/src/types/commonProps.tsx
@@ -7,5 +7,17 @@ import React from "react";
 export interface ButtonProps {
     width: string;
     height?: string;
-    onClick: (event: React.MouseEvent<HTMLElement>) => void;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export interface TextInputProps {
+    width: string;
+    id: string;
+    setTextValue: React.Dispatch<React.SetStateAction<string>>;
+    height?: string;
+    placeholder?: string;
+    defaultValue?: string;
+    labelName?: string;
+    svgSRC?: string;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
 }
