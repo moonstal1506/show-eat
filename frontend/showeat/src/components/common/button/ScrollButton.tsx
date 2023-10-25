58395705753540c07d491b81e65eed7466dd1cdc
Crassula1994 <gogun3535@gmail.com>
2023-10-25 15:49:38 +0900

    Feat: Add scroll-to-top logic
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/src/components/common/button/ScrollButton.tsx
+++ frontend/showeat/src/components/common/button/ScrollButton.tsx
@@ -2,63 +2,108 @@
 import { ButtonProps } from "@/types/commonProps";
 import Image from "next/image";
 import styled from "@emotion/styled";
-import { useState } from "react";
-
-// ----------------------------------------------------------------------------------------------------
-
-/* Type */
-interface ScrollButtonProps extends ButtonProps {}
+import { useEffect, useState } from "react";
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Style */
 const ButtonContainer = styled("div")`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5vh;
+    // Position Attribute
     position: fixed;
     right: 5%;
     bottom: 5%;
-    margin: 1%;
     z-index: 1;
-    align-items: center;
-    cursor: pointer;
-    user-select: none;
-`;
 
-const ImageWrapper = styled("div")`
-    max-width: 50px;
-    max-height: 50px;
-    position: relative;
+    // Layout Attribute
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5vh;
+
+    // Box Model Attribute
+    margin: 1%;
+
+    // Style Attribute
     img {
         filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.5));
     }
+
+    // Interaction Attribute
+    cursor: pointer;
+    user-select: none;
+    transition: all 0.3s ease;
+    &:hover {
+        transform: scale(1.1);
+        img {
+            filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.25));
+        }
+    }
+`;
+
+const ImageWrapper = styled("div")`
+    // Position Attribute
+    position: relative;
+
+    // Box Model Attribute
+    max-width: 50px;
+    max-height: 50px;
 `;
 
 const TextWrapper = styled("div")`
+    // Text Attribute
     font-weight: 700;
+    color: ${(props) => props.theme.colors.secondary3};
+    text-shadow:
+        1px 0px 3px rgba(0, 0, 0, 0.1),
+        -1px 0px 3px rgba(0, 0, 0, 0.1),
+        0px 0px 3px rgba(0, 0, 0, 0.1);
 `;
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Scroll Button Component */
-function ScrollButton(props: ScrollButtonProps) {
-    const { width, height = "auto", onClick } = props;
-    const [showButton, setShowButton] = useState<boolean>(true);
-    // const
+function ScrollButton(props: ButtonProps) {
+    // States and Variables
+    const { width, height = "auto" } = props;
+    const buttonStyle = {
+        width,
+        height,
+    };
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    // Functions
+    const scrollToTop = () => {
+        if (isVisible)
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+    };
+
+    // Hooks
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) setIsVisible(true);
+            else setIsVisible(false);
+        };
+
+        window.addEventListener("scroll", toggleVisibility);
+
+        return () => {
+            window.removeEventListener("scroll", toggleVisibility);
+        };
+    }, []);
+
     return (
-        showButton && (
-            <ButtonContainer onClick={onClick}>
-                <ImageWrapper width={width} height={height}>
+        isVisible && (
+            <ButtonContainer onClick={scrollToTop}>
+                <ImageWrapper>
                     <Image
                         src="/assets/icons/scroll-to-up-icon.svg"
                         width={50}
                         height={50}
                         alt="scroll-to-up"
-                        style={{
-                            width: "100%",
-                            height: "auto",
-                        }}
+                        style={buttonStyle}
                     />
                 </ImageWrapper>
                 <TextWrapper>맨 위로</TextWrapper>
