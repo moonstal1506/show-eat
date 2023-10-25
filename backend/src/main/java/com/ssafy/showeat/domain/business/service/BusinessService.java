52287943570eaa4f691e090f6d95f665d44dbba0
sujeong <msj1506@naver.com>
2023-10-25 11:06:19 +0900

    Feat: Add get seller info

--- backend/src/main/java/com/ssafy/showeat/domain/business/service/BusinessService.java
+++ backend/src/main/java/com/ssafy/showeat/domain/business/service/BusinessService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import com.ssafy.showeat.domain.business.dto.request.BusinessUserRequestDto;
+import com.ssafy.showeat.domain.business.dto.response.SellerResponseDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.showeat.domain.business.dto.request.RegistMenuRequestDto;
@@ -14,4 +15,5 @@ public interface BusinessService {
 	BusinessMenuResponseDto getMenuInfo(Long menuId);
 	List<BusinessMenuResponseDto> getMenuList();
     void registerBusinessUser(BusinessUserRequestDto businessUserRequestDto, MultipartFile businessRegistration, MultipartFile bankBook) throws IOException;
+	SellerResponseDto getSellerInfo(Long businessId);
 }
