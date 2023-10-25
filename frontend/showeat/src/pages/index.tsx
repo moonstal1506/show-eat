7d7aa90244a9393f97cde99ea69f5780f960d0a5
Crassula1994 <gogun3535@gmail.com>
2023-10-25 15:53:10 +0900

    Chore: Delete test code
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/src/pages/index.tsx
+++ frontend/showeat/src/pages/index.tsx
@@ -1,6 +1,5 @@
 /* Import */
 import Head from "next/head";
-import { ScrollButton } from "@components/common/button";
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -12,79 +11,7 @@ function Home() {
                 <title>쑈잇: ShowEat</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <div>구본웅, 그는 신인가!</div>
-            <ScrollButton width="100px" height="100px" onClick={() => {}} />
+            <main>구본웅, 그는 신인가!</main>
         </>
     );
 }
