7a1ffca293a9d563c9ab83de2fbae47fbdcd1868
sujeong <msj1506@naver.com>
2023-10-25 12:15:39 +0900

    Feat: Add get registration info

--- backend/src/main/java/com/ssafy/showeat/domain/business/service/BusinessService.java
+++ backend/src/main/java/com/ssafy/showeat/domain/business/service/BusinessService.java
@@ -5,14 +5,11 @@ import java.util.List;
 
 import com.ssafy.showeat.domain.business.dto.request.BusinessUserRequestDto;
 
-import com.ssafy.showeat.domain.business.dto.response.SellerResponseDto;
+import com.ssafy.showeat.domain.business.dto.response.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.showeat.domain.business.dto.request.RegistMenuRequestDto;
-import com.ssafy.showeat.domain.business.dto.response.BusinessMenuResponseDto;
-import com.ssafy.showeat.domain.business.dto.response.BusinessMonthlyStatResponseDto;
-import com.ssafy.showeat.domain.business.dto.response.BusinessTotalStatResponseDto;
 
 public interface BusinessService {
 	void registMenu(RegistMenuRequestDto registMenuRequestDto, List<MultipartFile> multipartFiles) throws IOException;
@@ -21,9 +18,7 @@ public interface BusinessService {
 
 	List<BusinessMenuResponseDto> getMenuList();
 
-    void registerBusinessUser(BusinessUserRequestDto businessUserRequestDto, MultipartFile businessRegistration, MultipartFile bankBook) throws IOException;
 	SellerResponseDto getSellerInfo(Long businessId);
-
 
 	List<BusinessMonthlyStatResponseDto> getMonthlyStatList(Long businessId);
 
@@ -32,4 +27,5 @@ public interface BusinessService {
 	void registerBusinessUser(BusinessUserRequestDto businessUserRequestDto, MultipartFile businessRegistration,
 		MultipartFile bankBook) throws IOException;
 
+	RegistrationResponseDto getRegistrationInfo(Long businessId);
 }
