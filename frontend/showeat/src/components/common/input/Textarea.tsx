6f5c7fcd673e6c0ac334b37a0b335ce5df15856f
Narn99 <vkdldjtmflr@naver.com>
2023-10-26 15:14:19 +0900

    Feat: Add textarea box and wrapper

--- frontend/showeat/src/components/common/input/Textarea.tsx
+++ frontend/showeat/src/components/common/input/Textarea.tsx
@@ -1,35 +1,66 @@
-import { useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
+import { TextInputProps } from "@customTypes/commonProps";
 
-function Textarea({ width, height, mexLength, textareaName }) {
-    const theme = useTheme();
+// ----------------------------------------------------------------------------------------------------
 
-    const TextareaContainer = styled("div")`
-        background-color: ${theme.colors.primary3};
-    `;
+/* Type */
+interface TextareaProps extends TextInputProps {
+    maxLength: number;
+    textareaName: string;
+}
 
-    const TextareaWrapper = styled("textarea")`
-        background-color: ${theme.colors.secondary3};
-        border-radius: 20px;
-        padding: 20px 30px;
-        resize: none;
-    `;
+const TextareaContainer = styled("div")`
+    background-color: ${(props) => props.theme.colors.primary3};
+`;
 
-    const TextareaLabelWrapper = styled("label")`
-        font-size: 20px;
-        font-weight: 700;
-    `;
+const TextareaWrapper = styled("div")<{ width: string; height: string | undefined }>`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+
+    background-color: ${(props) => props.theme.colors.secondary3};
+
+    padding: 20px 30px;
+    border-radius: 20px;
+`;
+
+const TextareaBox = styled("textarea")<{ width: string; height: string | undefined }>`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+
+    border: none;
+
+    resize: none;
+
+    &:focus,
+    &:active {
+        outline: none;
+    }
+`;
+
+const TextareaLabelWrapper = styled("label")`
+    font-size: 20px;
+    font-weight: 700;
+`;
+
+function Textarea({ width, height, maxLength, id, textareaName, setTextValue }: TextareaProps) {
+    const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newValue = e.target.value;
+        setTextValue(newValue);
+    };
 
     return (
         <TextareaContainer>
             <TextareaLabelWrapper>{textareaName}</TextareaLabelWrapper>
-            <TextareaWrapper
-                id={id}
-                name={id}
-                width={width}
-                height={height}
-                maxLength={maxLength}
-            />
+            <TextareaWrapper width={width} height={height}>
+                <TextareaBox
+                    id={id}
+                    name={id}
+                    width={width}
+                    height={height}
+                    maxLength={maxLength}
+                    onChange={(e) => handleChangeText(e)}
+                />
+            </TextareaWrapper>
         </TextareaContainer>
     );
 }
--- frontend/showeat/src/pages/index.tsx
+++ frontend/showeat/src/pages/index.tsx
@@ -1,11 +1,16 @@
 /* Import */
 import Textarea from "@/components/common/input/Textarea";
 import Head from "next/head";
+import { useState } from "react";
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Home Component */
 function Home() {
+    const [textValue, setTextValue] = useState("");
+
+    console.log(textValue);
+
     return (
         <>
             <Head>
@@ -14,7 +19,14 @@ function Home() {
             </Head>
             <main>
                 구건, 그는 신인가!
-                <Textarea />
+                <Textarea
+                    id="review"
+                    width="200px"
+                    height="100px"
+                    maxLength={200}
+                    textareaName="안녕하세요"
+                    setTextValue={setTextValue}
+                />
             </main>
         </>
     );
