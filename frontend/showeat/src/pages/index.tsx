a83f3a73eb0e835e1c8a9f2ffa25d4b7f73de894
Narn99 <vkdldjtmflr@naver.com>
2023-10-26 17:25:30 +0900

    Chore: Restore index.tsx

--- frontend/showeat/src/pages/index.tsx
+++ frontend/showeat/src/pages/index.tsx
@@ -1,33 +1,17 @@
 /* Import */
-import Textarea from "@/components/common/input/Textarea";
 import Head from "next/head";
-import { useState } from "react";
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Home Component */
 function Home() {
-    const [textValue, setTextValue] = useState("");
-
-    console.log(textValue);
-
     return (
         <>
             <Head>
                 <title>쑈잇: ShowEat</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <main>
-                구건, 그는 신인가!
-                <Textarea
-                    id="review"
-                    width="200px"
-                    height="100px"
-                    maxLength={200}
-                    textareaName="안녕하세요"
-                    setTextValue={setTextValue}
-                />
-            </main>
+            <main>구건, 그는 신인가!</main>
         </>
     );
 }
