c65b386e29fd075b79a0209dc4203c034c65147b
Narn99 <vkdldjtmflr@naver.com>
2023-10-26 17:12:52 +0900

    Feat: Add textarea component
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/components/common/input/Textarea.tsx
+++ frontend/showeat/src/components/common/input/Textarea.tsx
@@ -1,3 +1,4 @@
+/* Import */
 import styled from "@emotion/styled";
 import { TextInputProps } from "@customTypes/commonProps";
 
@@ -7,27 +8,66 @@ import { TextInputProps } from "@customTypes/commonProps";
 interface TextareaProps extends TextInputProps {
     maxLength: number;
     textareaName: string;
+    focusColor: "primary" | "secondary" | "gray";
+    // eslint-disable-next-line react/require-default-props
+    fontSize?: string;
+    // eslint-disable-next-line react/require-default-props
+    labelFontSize?: string;
 }
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Style */
 const TextareaContainer = styled("div")`
-    background-color: ${(props) => props.theme.colors.primary3};
+    border: 1px solid ${(props) => props.theme.colors.gray2};
 `;
 
-const TextareaWrapper = styled("div")<{ width: string; height: string | undefined }>`
+const TextareaWrapper = styled("div")<{
+    width: string;
+    height: string | undefined;
+    focusColor: "primary" | "secondary" | "gray";
+}>`
     width: ${(props) => props.width};
     height: ${(props) => props.height};
 
-    background-color: ${(props) => props.theme.colors.secondary3};
-
     padding: 20px 30px;
+    margin-top: 5px;
+
+    border: 1px solid ${(props) => props.theme.colors.gray2};
     border-radius: 20px;
+
+    &:focus-within {
+        box-shadow: 0px 0px 4px 2px
+            ${(props) =>
+                // eslint-disable-next-line no-nested-ternary
+                props.focusColor === "primary"
+                    ? props.theme.colors.primary2
+                    : props.focusColor === "secondary"
+                    ? props.theme.colors.secondary2
+                    : props.theme.colors.gray2};
+        outline: 1px solid
+            ${(props) =>
+                // eslint-disable-next-line no-nested-ternary
+                props.focusColor === "primary"
+                    ? props.theme.colors.primary3
+                    : props.focusColor === "secondary"
+                    ? props.theme.colors.secondary3
+                    : props.theme.colors.gray3};
+    }
 `;
 
-const TextareaBox = styled("textarea")<{ width: string; height: string | undefined }>`
+const TextareaBox = styled("textarea")<{
+    width: string;
+    height: string | undefined;
+    fontSize?: string;
+}>`
     width: ${(props) => props.width};
     height: ${(props) => props.height};
 
     border: none;
+    background-color: transparent;
+
+    font-size: ${(props) => (props.fontSize ? props.fontSize : "14px")};
 
     resize: none;
 
@@ -37,12 +77,25 @@ const TextareaBox = styled("textarea")<{ width: string; height: string | undefin
     }
 `;
 
-const TextareaLabelWrapper = styled("label")`
-    font-size: 20px;
+const TextareaLabelWrapper = styled("label")<{ labelFontSize?: string }>`
+    font-size: ${(props) => (props.labelFontSize ? props.labelFontSize : "20px")};
     font-weight: 700;
 `;
 
-function Textarea({ width, height, maxLength, id, textareaName, setTextValue }: TextareaProps) {
+// ----------------------------------------------------------------------------------------------------
+
+/* Textarea Button Component */
+function Textarea({
+    width,
+    height,
+    maxLength,
+    id,
+    textareaName,
+    setTextValue,
+    focusColor,
+    fontSize,
+    labelFontSize,
+}: TextareaProps) {
     const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const newValue = e.target.value;
         setTextValue(newValue);
@@ -50,8 +103,10 @@ function Textarea({ width, height, maxLength, id, textareaName, setTextValue }: 
 
     return (
         <TextareaContainer>
-            <TextareaLabelWrapper>{textareaName}</TextareaLabelWrapper>
-            <TextareaWrapper width={width} height={height}>
+            <TextareaLabelWrapper labelFontSize={labelFontSize}>
+                {textareaName}
+            </TextareaLabelWrapper>
+            <TextareaWrapper width={width} height={height} focusColor={focusColor}>
                 <TextareaBox
                     id={id}
                     name={id}
@@ -59,10 +114,14 @@ function Textarea({ width, height, maxLength, id, textareaName, setTextValue }: 
                     height={height}
                     maxLength={maxLength}
                     onChange={(e) => handleChangeText(e)}
+                    fontSize={fontSize}
                 />
             </TextareaWrapper>
         </TextareaContainer>
     );
 }
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Export */
 export default Textarea;
