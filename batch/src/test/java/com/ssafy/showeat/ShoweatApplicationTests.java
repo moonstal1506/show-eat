141eb57e121a647cc28e8a2f7b3521d15e0f8a79
psg980331@naver.com <psg980331@naver.com>
2023-10-27 13:50:25 +0900

    Feat: Create batch server

---
+++ batch/src/test/java/com/ssafy/showeat/ShoweatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.showeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoweatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
