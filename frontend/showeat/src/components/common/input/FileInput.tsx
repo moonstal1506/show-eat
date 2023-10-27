33d7c76dfe08a7e964553653a8c7278c86783548
Narn99 <vkdldjtmflr@naver.com>
2023-10-27 16:59:13 +0900

    Feat: Add file input component
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/components/common/input/FileInput.tsx
+++ frontend/showeat/src/components/common/input/FileInput.tsx
@@ -1,28 +1,43 @@
+/* Import */
 import { ButtonProps } from "@/customTypes/commonProps";
-import { useRef } from "react";
+import fontWeightChanger from "@/utils/fontWeightChanger";
+import { useRef, useState } from "react";
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/react";
+import Image from "next/image";
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Type */
 interface FileInputProps {
     count: 1 | 2 | 3 | 4 | 5;
     color: "primary" | "secondary" | "gray" | "white";
     id: string;
     buttonWidth: string;
     buttonHeight: string;
-    // eslint-disable-next-line react/require-default-props
     buttonFontSize?: string;
-    // eslint-disable-next-line react/require-default-props
     buttonFontWeight?: string;
-    // eslint-disable-next-line react/require-default-props
-    labelName?: string;
+    buttonDescription: string;
+    listWidth?: string;
+    listHeight?: string;
 }
 
-interface FileInputButtonProps extends ButtonProps {
+interface FileInputButtonWrapperTypes extends ButtonProps {
     fontSize: string;
     fontWeight: string;
     color: "primary" | "secondary" | "gray" | "white";
 }
 
+interface FilesListContainerTypes {
+    listWidth: string;
+    listHeight?: string;
+    addIcon: boolean;
+    color: "primary" | "secondary" | "gray" | "white";
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+/* Style */
 const clickAnimation = keyframes`
 from {
   transform: scale(1)
@@ -36,34 +51,44 @@ const FileInputContainer = styled("div")`
     //
 `;
 
+const FileInputButtonContainer = styled("div")`
+    display: flex;
+    align-items: center;
+`;
+
 const FileInputWrapper = styled("input")`
     display: none;
 `;
 
-const FileInputButtonWrapper = styled("button")<FileInputButtonProps>`
+const FileInputCountTextWrapper = styled("span")`
+    margin-left: 10px;
+    font-size: 14px;
+`;
+
+const FileInputButtonWrapper = styled("button")<FileInputButtonWrapperTypes>`
     width: ${(props) => props.width};
     height: ${(props) => props.height};
 
     font-size: ${(props) => props.fontSize};
     font-weight: ${(props) => props.fontWeight};
+    color: ${(props) => {
+        if (props.color === "secondary" || props.color === "primary") {
+            return "white";
+        }
+        return "black";
+    }};
 
     background-color: ${(props) => {
-        let buttonColor = "";
-
-        if (props.color === "gray") {
-            buttonColor = `${props.color}1`;
-        } else {
-            buttonColor = `${props.color}3`;
-        }
-
         if (props.color !== "white") {
-            return props.theme.colors[buttonColor];
+            return props.theme.colors[`${props.color}3`];
         }
         return "white";
     }};
 
     border: ${(props) => (props.color === "white" ? "1px solid black" : "none")};
     border-radius: 10px;
+    margin-right: 10px;
+
     cursor: pointer;
 
     text-align: center;
@@ -78,24 +103,15 @@ const FileInputButtonWrapper = styled("button")<FileInputButtonProps>`
 
     &:hover {
         background-color: ${(props) => {
-            let buttonColor = "";
-
-            if (props.color === "gray") {
-                buttonColor = `${props.color}2`;
-            } else {
-                buttonColor = `${props.color}4`;
-            }
-
             if (props.color !== "white") {
-                return props.theme.colors[buttonColor];
+                return props.theme.colors[`${props.color}4`];
             }
-            // colorNum에 따라 다른 배경색을 설정
             return "#d7d7d7";
         }};
         box-shadow: 0px 0px 5px 2px ${(props) => props.theme.colors.gray3};
 
         color: ${(props) => {
-            if (props.color === "white" || props.color === "gray") {
+            if (props.color === "white") {
                 return "black";
             }
             return "white";
@@ -105,16 +121,8 @@ const FileInputButtonWrapper = styled("button")<FileInputButtonProps>`
 
     &:active {
         background-color: ${(props) => {
-            let buttonColor = "";
-
-            if (props.color === "gray") {
-                buttonColor = `${props.color}3`;
-            } else {
-                buttonColor = `${props.color}5`;
-            }
-
             if (props.color !== "white") {
-                return props.theme.colors[buttonColor];
+                return props.theme.colors[`${props.color}5`];
             }
 
             return "#a7a7a7";
@@ -122,7 +130,7 @@ const FileInputButtonWrapper = styled("button")<FileInputButtonProps>`
         box-shadow: 0px 0px 5px 2px ${(props) => props.theme.colors.gray4};
 
         color: ${(props) => {
-            if (props.color === "white" || props.color === "gray") {
+            if (props.color === "white") {
                 return "black";
             }
             return "white";
@@ -133,10 +141,85 @@ const FileInputButtonWrapper = styled("button")<FileInputButtonProps>`
     }
 `;
 
-const FileInputLabelWrapper = styled("label")`
-    font-size: 20px;
+const FilesListContainer = styled("div")<FilesListContainerTypes>`
+    width: ${(props) => props.listWidth};
+    height: ${(props) => props.listHeight};
+
+    display: flex;
+    justify-content: ${(props) => (props.addIcon ? "center" : "space-between")};
+    align-items: center;
+
+    margin: 10px 0px;
+    padding: 10px 20px;
+
+    border: 1px solid #c7c7c7;
+    border-radius: 20px;
+
+    box-shadow: 0px 0px 5px 1px ${(props) => props.theme.colors.gray2};
+
+    &.dragging-over {
+        box-shadow: 0px 0px 8px 2px ${(props) => props.theme.colors[`${props.color}2`]};
+    }
 `;
 
+const FileImageContainer = styled("div")`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FileImageWrapper = styled(Image)`
+    border: 1px solid #c7c7c7;
+    border-radius: 10px;
+    cursor: grab;
+
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    &:hover {
+        transform: scale(1.1);
+        filter: drop-shadow(0 0 5px ${(props) => props.theme.colors.gray3});
+    }
+
+    &:active {
+        cursor: grabbing;
+    }
+`;
+
+const DeleteIconWrapper = styled(Image)`
+    align-self: flex-end;
+    cursor: pointer;
+
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    &:hover {
+        transform: scale(1.1);
+        filter: drop-shadow(0 0 5px ${(props) => props.theme.colors.gray3});
+    }
+`;
+
+const AddIconWrapper = styled(Image)`
+    cursor: pointer;
+
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    &:hover {
+        transform: scale(1.1);
+        filter: drop-shadow(0 0 5px ${(props) => props.theme.colors.gray3});
+    }
+`;
+
+// ----------------------------------------------------------------------------------------------------
+
+/* File Input Component */
 function FileInput({
     count,
     id,
@@ -145,9 +228,13 @@ function FileInput({
     buttonHeight,
     buttonFontSize = "20px",
     buttonFontWeight = "Regular",
-    labelName,
+    buttonDescription,
+    listWidth = `${count * 100}px`,
+    listHeight = "80px",
 }: FileInputProps) {
     const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
 
     const handleInputButton = () => {
         if (inputRef.current) {
@@ -155,29 +242,147 @@ function FileInput({
         }
     };
 
+    const handleUploadFiles = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = e.target;
+
+        if (files) {
+            const fileList = Array.from(files);
+
+            if (fileList.length + uploadedFiles.length <= count) {
+                setUploadedFiles([...uploadedFiles, ...fileList]);
+            } else {
+                // 다른거로 표시해줘야 할 듯
+                console.log(`이미지는 ${count}개까지만 올릴 수 있습니다.`);
+            }
+        }
+    };
+
+    const handleDragOver = (e: React.DragEvent) => {
+        e.preventDefault();
+        e.currentTarget.classList.add("dragging-over");
+    };
+
+    function handleDragLeave(e: React.DragEvent) {
+        e.currentTarget.classList.remove("dragging-over");
+    }
+
+    function handleDragEnd(e: React.DragEvent) {
+        e.currentTarget.classList.remove("dragging-over");
+    }
+
+    const handleDrop = (e: React.DragEvent) => {
+        e.preventDefault();
+        const fileList = e.dataTransfer.files;
+
+        if (fileList.length + uploadedFiles.length <= count) {
+            setUploadedFiles([...uploadedFiles, ...Array.from(fileList)]);
+        } else {
+            // 다른거로 표시해줘야 할 듯
+            console.log(`이미지는 ${count}개까지만 올릴 수 있습니다.`);
+        }
+        e.currentTarget.classList.remove("dragging-over");
+    };
+
+    const handleDeleteIcon = (fileIndex: number) => {
+        const updatedFiles = uploadedFiles.filter((file, index) => index !== fileIndex);
+
+        setUploadedFiles(updatedFiles);
+    };
+
+    const handleAddIcon = () => {
+        if (inputRef.current) {
+            inputRef.current.click();
+        }
+    };
+
+    const countText = fontWeightChanger(`...${count}개...까지 업로드 가능합니다.`);
+
     return (
         <FileInputContainer>
-            {labelName && <FileInputLabelWrapper htmlFor={id}>{labelName}</FileInputLabelWrapper>}
-            <FileInputButtonWrapper
-                width={buttonWidth}
-                height={buttonHeight}
-                fontSize={buttonFontSize}
-                fontWeight={buttonFontWeight}
-                onClick={handleInputButton}
-                color={color}
-            >
-                안녕하세요
-            </FileInputButtonWrapper>
-            <FileInputWrapper
-                ref={inputRef}
-                id={id}
-                name={id}
-                type="file"
-                accept="image/*"
-                multiple={count !== 1}
-            />
+            <FileInputButtonContainer>
+                <FileInputButtonWrapper
+                    width={buttonWidth}
+                    height={buttonHeight}
+                    fontSize={buttonFontSize}
+                    fontWeight={buttonFontWeight}
+                    onClick={handleInputButton}
+                    color={color}
+                >
+                    {buttonDescription}
+                </FileInputButtonWrapper>
+                {count === 1 && uploadedFiles.length === 1 && (
+                    <FileImageContainer>
+                        <FileImageWrapper
+                            src={URL.createObjectURL(uploadedFiles[0])}
+                            alt="uploaded-image"
+                            width="60"
+                            height="60"
+                            draggable={false}
+                        />
+                        <DeleteIconWrapper
+                            src="/assets/icons/delete-icon.svg"
+                            alt="uploaded-image-delete-icon"
+                            width={20}
+                            height={20}
+                            onClick={() => handleDeleteIcon(0)}
+                        />
+                    </FileImageContainer>
+                )}
+                <FileInputCountTextWrapper dangerouslySetInnerHTML={{ __html: countText }} />
+                <FileInputWrapper
+                    ref={inputRef}
+                    id={id}
+                    name={id}
+                    type="file"
+                    accept="image/*"
+                    multiple={count !== 1}
+                    onChange={(e) => handleUploadFiles(e)}
+                />
+            </FileInputButtonContainer>
+            {count !== 1 && (
+                <FilesListContainer
+                    listWidth={listWidth}
+                    listHeight={listHeight}
+                    onDragOver={(e) => handleDragOver(e)}
+                    onDragLeave={(e) => handleDragLeave(e)}
+                    onDragEnd={(e) => handleDragEnd(e)}
+                    onDrop={(e) => handleDrop(e)}
+                    addIcon={uploadedFiles.length === 0}
+                    color={color}
+                >
+                    {uploadedFiles.length === 0 && (
+                        <AddIconWrapper
+                            src="/assets/icons/round_add_icon.svg"
+                            alt="add-icon"
+                            width={40}
+                            height={40}
+                            onClick={handleAddIcon}
+                        />
+                    )}
+                    {uploadedFiles.map((file, fileIndex) => (
+                        <FileImageContainer key={`file-input-${file.name}-${fileIndex + 1}`}>
+                            <FileImageWrapper
+                                src={URL.createObjectURL(file)}
+                                alt={file.name}
+                                width="60"
+                                height="60"
+                            />
+                            <DeleteIconWrapper
+                                src="/assets/icons/delete-icon.svg"
+                                alt={`${file.name}-delete-icon`}
+                                width={20}
+                                height={20}
+                                onClick={() => handleDeleteIcon(fileIndex)}
+                            />
+                        </FileImageContainer>
+                    ))}
+                </FilesListContainer>
+            )}
         </FileInputContainer>
     );
 }
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Export */
 export default FileInput;
