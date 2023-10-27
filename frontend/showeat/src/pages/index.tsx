538c648a8efe44619fd878a17481f3124c38ed68
Crassula1994 <gogun3535@gmail.com>
2023-10-27 12:26:51 +0900

    Chore: Delete test code
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/src/pages/index.tsx
+++ frontend/showeat/src/pages/index.tsx
@@ -1,7 +1,5 @@
 /* Import */
 import Head from "next/head";
-import { TextButton } from "@components/common/button";
-import { RightArrowIcon } from "public/assets/icons";
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -14,15 +12,6 @@ function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <main>구건, 그는 신인가!</main>
-            <TextButton width="150px" text="확인" fontSize={16} />
-            <TextButton width="150px" text="확인" />
-            <TextButton width="150px" text="취소" fill="negative" />
-            <TextButton width="150px" text="확인" colorType="secondary" />
-            <TextButton width="150px" text="취소" colorType="secondary" fill="negative" />
-            <TextButton width="150px" text="확인" colorType="gray" />
-            <TextButton width="150px" text="취소" colorType="gray" fill="negative" />
-            <TextButton width="150px" text="공유" icon={<RightArrowIcon />} />
-            <RightArrowIcon />
         </>
     );
 }
