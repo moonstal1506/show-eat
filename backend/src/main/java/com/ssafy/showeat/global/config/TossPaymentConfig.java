1d4da10ddd5ddb7a0dfcd110a424ca0aa57ec08c
Haaarimmm <haaarimmm@gmail.com>
2023-10-31 16:00:44 +0900

    Fix: Modify payment config

--- backend/src/main/java/com/ssafy/showeat/global/config/TossPaymentConfig.java
+++ backend/src/main/java/com/ssafy/showeat/global/config/TossPaymentConfig.java
@@ -9,12 +9,12 @@ import lombok.Getter;
 @Configuration
 public class TossPaymentConfig {
 
-	@Value("${payment.toss.test_client_api_key}")
+	@Value("${payments.toss.test_client_api_key}")
 	private String testClientApiKey;
 
-	@Value("${payment.toss.test_secret_api_key}")
+	@Value("${payments.toss.test_secret_api_key}")
 	private String testSecretApiKey;
 
-	public static final String TossUrl = "https://api.tosspayments.com/v1/payments/";
+	public static final String TossOriginUrl = "https://api.tosspayments.com/v1/payments/";
 
 }
