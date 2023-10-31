3c9252d8ec1cb02860f4446bdcbb63229d7d0793
Narn99 <vkdldjtmflr@naver.com>
2023-10-31 11:08:51 +0900

    Feat: Add button drop down animation

--- frontend/showeat/src/components/common/dropdown/ButtonDropDown.tsx
+++ frontend/showeat/src/components/common/dropdown/ButtonDropDown.tsx
@@ -18,18 +18,22 @@ interface ButtonDropDownProps {
 const dropDownFadein = keyframes`
         from {
             opacity: 0;
+            transform: translateY(-10px);
         }
         to {
             opacity: 1;
+            transform: translateY(0px);
         }
 `;
 
 const dropDownFadeOut = keyframes`
         from {
             opacity: 1;
+            transform: translateY(0px);
         }
         to {
             opacity: 0;
+            transform: translateY(-10px);
         }
 `;
 
