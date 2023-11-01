45652657d225c518393e05c61a9af12f003af9cc
Crassula1994 <gogun3535@gmail.com>
2023-11-01 17:39:50 +0900

    Refactor: Modify main layout
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/src/layouts/MainLayout.tsx
+++ frontend/showeat/src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 /* Import */
 import { LayoutProps } from "@customTypes/layoutProps";
+import Header from "@components/header";
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -9,6 +10,7 @@ function MainLayout(props: LayoutProps) {
 
     return (
         <>
+            <Header />
             <div>여기는 메인 페이지입니다.</div>
             <article>{children}</article>
         </>
