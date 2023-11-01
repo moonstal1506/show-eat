28777f7e65ea8b0e888a6ff261c8bcede635381f
psg980331@naver.com <psg980331@naver.com>
2023-11-01 10:02:25 +0900

    Refactor: Modify concurrency test class

--- backend/src/test/java/com/ssafy/showeat/domain/funding/service/FundingServiceImplTest.java
+++ backend/src/test/java/com/ssafy/showeat/domain/funding/service/FundingServiceImplTest.java
@@ -59,9 +59,6 @@ class FundingServiceImplTest extends IntegrationTestSupport {
 	@Autowired
 	private CredentialRepository credentialRepository;
 
-	@Autowired
-	private BusinessMenuRepository businessMenuRepository;
-
 	@AfterEach
 	void reset() {
 		fundingRepository.deleteAll();
@@ -88,7 +85,7 @@ class FundingServiceImplTest extends IntegrationTestSupport {
 			.builder()
 			.title("테스트")
 			.description("테스트입니다.")
-			.category("한식")
+			.category("KOREAN")
 			.maxLimit(10)
 			.minLimit(5)
 			.tags(List.of("tag1","tag2"))
@@ -112,40 +109,6 @@ class FundingServiceImplTest extends IntegrationTestSupport {
 		assertThat(save.getFundingImages()).hasSize(2)
 			.extracting("fundingImgUrl")
 			.containsExactlyInAnyOrder("img1","img2");
-	}
-
-	@Test
-	@DisplayName("펀딩에 동시에 100명이 참여하는경우 동시성 문제가 발생하지 않는다.")
-	void 동시에_100명이_참여() throws InterruptedException {
-	    // given
-		Funding funding = createFunding(FundingIsActive.ACTIVE);
-		Funding save = fundingRepository.save(funding);
-		createUsers();
-		int threadCount = 100;
-
-		//멀티 쓰레드를 사용하기 위함, 비동기 작업을 단순화 해줌
-		ExecutorService executorService = Executors.newFixedThreadPool(32);
-		//다른쓰레드의 작업이 끝날때까지 기다림
-		CountDownLatch latch = new CountDownLatch(threadCount);
-
-		// when
-		for (int i = 1; i <= threadCount; i++) {
-			int finalI = i;
-			executorService.submit(() -> {
-				try {
-					User user = userRepository.findById((long)finalI).get();
-					fundingService.applyFunding(save.getFundingId(),user);
-				} finally {
-					latch.countDown();
-				}
-			});
-		}
-
-		latch.await();
-		Funding resultFunding = fundingRepository.findById(save.getFundingId()).get();
-
-		// then
-		assertThat(resultFunding.getFundingCurCount()).isEqualTo(100);
 	}
 
 	@Test
