1a4eb977538730aad55c873ad9b1e3dee64acb63
Crassula1994 <gogun3535@gmail.com>
2023-11-03 17:10:00 +0900

    Chore: Add image domains setting
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/next.config.js
+++ frontend/showeat/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
+    images: {
+        domains: ["k.kakaocdn.net", "localhost"],
+    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/,
