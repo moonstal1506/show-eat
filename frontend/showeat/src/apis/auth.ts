7606eac90fe289777babfec5944a02eaa7e91ef7
Crassula1994 <gogun3535@gmail.com>
2023-11-03 17:12:45 +0900

    Feat: Modify function for authentification with Kakao API
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/src/apis/auth.ts
+++ frontend/showeat/src/apis/auth.ts
@@ -1,6 +1,10 @@
+/* Import */
 import { fetchGet } from "@utils/api";
 import { FetchProps } from "@customTypes/apiProps";
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Function for Authentificaion with Kakao API */
 const authWithKakao = async (code: string) => {
     const props: FetchProps = {
         url: "auth/login/kakao",
@@ -8,10 +12,12 @@ const authWithKakao = async (code: string) => {
         isAuth: false,
         params: { code },
     };
-
     const result = await fetchGet(props);
 
-    console.log(result.data.tokenDto.accessToken);
+    return result;
 };
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Export */
 export default authWithKakao;
