f51ff8b6bbfb1876d39fb5dae16281faafa220d4
Narn99 <vkdldjtmflr@naver.com>
2023-11-04 23:59:13 +0900

    Feat: Add react slick library for carousel and create carousel component
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/pages/index.tsx
+++ frontend/showeat/src/pages/index.tsx
@@ -1,19 +1,65 @@
 /* Import */
 import Head from "next/head";
+import styled from "@emotion/styled";
 import MainLayout from "@layouts/MainLayout";
 import { ReactNode } from "react";
+import SearchBar from "@/components/searchBar/SearchBar";
+import Carousel from "../components/carousel/Carousel";
 
 // ----------------------------------------------------------------------------------------------------
 
+const AdvertiseContainer = styled("div")`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 300px;
+`;
+
+const MainContentsContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    min-width: 1020px;
+
+    padding-top: 20px;
+`;
+
+const RecommendContentsContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const CarouselWrapper = styled("div")`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
 /* Home Component */
 function Home() {
+    const isLogin = false;
     return (
         <>
             <Head>
                 <title>쑈잇: ShowEat</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <main>구건, 그는 신인가!</main>
+            <main>
+                <AdvertiseContainer>광고판</AdvertiseContainer>
+                <MainContentsContainer>
+                    <SearchBar />
+                    <RecommendContentsContainer>
+                        {isLogin && <span>억</span>}
+                        <CarouselWrapper>
+                            <Carousel />
+                        </CarouselWrapper>
+                    </RecommendContentsContainer>
+                </MainContentsContainer>
+            </main>
         </>
     );
 }
