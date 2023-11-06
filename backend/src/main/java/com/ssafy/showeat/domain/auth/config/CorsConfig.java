9a59b8a22f1277aaa83d6fb9723b4fa359567bb6
ooyouj <ooyouj@naver.com>
2023-11-06 12:18:22 +0900

    Fix: Modify cors config

--- backend/src/main/java/com/ssafy/showeat/domain/auth/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/showeat/domain/auth/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
