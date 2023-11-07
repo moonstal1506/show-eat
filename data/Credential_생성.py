b17160fbceaad9a2ed53a5293269a857c57b2be6
psg980331@naver.com <psg980331@naver.com>
2023-11-07 10:33:54 +0900

    Feat: Add credential dummy data to server db

--- data/Credential_생성.py
+++ data/Credential_생성.py
@@ -1,6 +1,7 @@
 import csv
 from faker import Faker
 import uuid
+from datetime import datetime
 
 # 더미 데이터 생성을 위한 Faker 객체 생성 (한국어로 설정)
 fake = Faker('ko_KR')
@@ -10,7 +11,7 @@ num_records = 100
 
 # 더미 데이터 생성 및 CSV 파일로 저장
 with open('credential.csv', 'w', newline='', encoding='cp949') as csvfile:
-    fieldnames = ['credential_id', 'email', 'credential_social_platform', 'credential_role']
+    fieldnames = ['credential_id', 'email', 'credential_social_platform', 'credential_role',"created_date","modified_date"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -20,6 +21,8 @@ with open('credential.csv', 'w', newline='', encoding='cp949') as csvfile:
             'email': fake.email(),
             'credential_social_platform': "kakao",
             'credential_role': "ADMIN",
+            'created_date' : datetime.now(),
+            'modified_date' : datetime.now()
         }
         writer.writerow(row)
 
