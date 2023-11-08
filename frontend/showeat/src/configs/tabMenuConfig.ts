107934eaba18c4801c6f63776ca8e5786c3bb4f1
Crassula1994 <gogun3535@gmail.com>
2023-11-08 16:00:15 +0900

    Feat: Add tab transition logic
    Co-authored-by: Narn99 <https://github.com/Narn99>

---
+++ frontend/showeat/src/configs/tabMenuConfig.ts
@@ -0,0 +1,10 @@
+/* Funding Tab Menu Config */
+const fundingTabMenuConfig = [
+    { id: "store", labelText: "셀러 정보", redirectUrl: "/fundings/[fundingId]/store" },
+    { id: "review", labelText: "리뷰", redirectUrl: "/fundings/[fundingId]/review" },
+];
+
+// ----------------------------------------------------------------------------------------------------
+
+/* Export */
+export default fundingTabMenuConfig;
--- frontend/showeat/src/pages/fundings/[fundingId]/[...params].tsx
+++ frontend/showeat/src/pages/fundings/[fundingId]/[...params].tsx
@@ -1,57 +1,58 @@
 /* Import */
-import FundingLayout from "@layouts/FundingLayout";
-import { ReactNode, useEffect, useState } from "react";
 import { Tab, TabBar } from "@/components/composite/tabBar";
+import fundingTabMenuConfig from "@configs/tabMenuConfig";
+import MainLayout from "@layouts/MainLayout";
+import { ReactNode, useState } from "react";
 import { useRouter } from "next/router";
+import withAuth from "@libs/withAuth";
 
 // ----------------------------------------------------------------------------------------------------
 
-/* Funding Page */
+/* Funding Tab Page */
 function FundingTab() {
+    // States and Variables
     const router = useRouter();
-    const { fundingId, tab: currentTab } = router.query;
-    const defaultTab = "review";
-    const fundingTabs = [
-        { labelText: "review", href: `/fundings/${fundingId}/review` },
-        { labelText: "store", href: `/fundings/${fundingId}/store` },
-    ];
-    const [activeTab, setActiveTab] = useState<string>((currentTab as string) || defaultTab);
-    const [number, setNumber] = useState<number>(0);
-    const handleTabClick = (labelText: string, href: string) => {
-        setActiveTab(labelText);
-        router.push(href, undefined, { shallow: true });
+    const { fundingId, tabName } = router.query;
+    const [activeTab, setActiveTab] = useState<string>((tabName as string) || "store");
+
+    // Function for Handling Tab Click
+    const handleTabClick = (id: string, redirectUrl: string) => {
+        const newRedirectUrl = redirectUrl.replace("[fundingId]", `${fundingId}`);
+        setActiveTab(id);
+        router.push(newRedirectUrl, undefined, { shallow: true });
     };
 
     return (
         <div>
             펀딩 메인 페이지입니다.
-            {number}
-            <button type="button" onClick={() => setNumber(number + 1)}>
-                더하기
-            </button>
             <TabBar>
-                {fundingTabs.map((tab) => (
+                {fundingTabMenuConfig.map((tab) => (
                     <Tab
-                        key={tab.labelText}
+                        key={tab.id}
                         labelText={tab.labelText}
-                        isActive={activeTab === tab.labelText}
-                        onClick={() => handleTabClick(tab.labelText, tab.href)}
+                        isActive={activeTab === tab.id}
+                        onClick={() => handleTabClick(tab.id, tab.redirectUrl)}
                     />
                 ))}
             </TabBar>
-            <div>{activeTab === "review" ? <div>리뷰</div> : <div>상점</div>}</div>
+            <div>{activeTab === "store" ? <div>상점</div> : <div>리뷰</div>}</div>
         </div>
     );
 }
 
 // ----------------------------------------------------------------------------------------------------
 
+/* Middleware */
+const FundingTabWithAuth = withAuth({ WrappedComponent: FundingTab });
+
+// ----------------------------------------------------------------------------------------------------
+
 /* Layout */
-FundingTab.getLayout = function getLayout(page: ReactNode) {
-    return <FundingLayout>{page}</FundingLayout>;
+FundingTabWithAuth.getLayout = function getLayout(page: ReactNode) {
+    return <MainLayout>{page}</MainLayout>;
 };
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Export */
-export default FundingTab;
+export default FundingTabWithAuth;
