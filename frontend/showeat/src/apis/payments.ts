4773352b54b3888b4510fa2d0ce140381b138595
Haaarimmm <haaarimmm@gmail.com>
2023-11-09 17:55:47 +0900

    Feat: Add payments api

--- frontend/showeat/src/apis/payments.ts
+++ frontend/showeat/src/apis/payments.ts
@@ -1,35 +1,36 @@
-// /* Import */
-// import { fetchModify } from "@utils/api";
-// import { FetchProps } from "@customTypes/apiProps";
+/* Import */
+import { fetchModify } from "@utils/api";
+import { FetchProps } from "@customTypes/apiProps";
 
-// // ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
 
-// /* Function for get Funding datas */
-// // interface GetPaymentPageType {
-// //     type: string;
-// // }
+/* Function for get Funding datas */
+// interface GetPaymentPageType {
+//     type: string;
+// }
 
-// /* Function for Bookmark Funding */
-// const postBookmark = async (
-//     payType: string,
-//     amount: number,
-//     orderName: string,
-//     credentialId: string,
-//     userEmail: string,
-// ) => {
-//     const props: FetchProps = {
-//         url: `payments/request`,
-//         method: "POST",
-//         data: { payType, amount, orderName, credentialId, userEmail, userNickname },
-//         isAuth: true,
-//     };
+/* Function for Bookmark Funding */
+const postRequestPayments = async (
+    payType: string,
+    amount: number,
+    orderName: string,
+    credentialId: string,
+    userEmail: string,
+    userNickname: string,
+) => {
+    const props: FetchProps = {
+        url: `payments/request`,
+        method: "POST",
+        data: { payType, amount, orderName, credentialId, userEmail, userNickname },
+        isAuth: true,
+    };
 
-//     const result = await fetchModify(props);
+    const result = await fetchModify(props);
 
-//     return result;
-// };
+    return result;
+};
 
-// // ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
 
-// /* Export */
-// export { getMainPageList, getMyFundings, getBookmarkFundings, postBookmark };
+/* Export */
+export default postRequestPayments;
