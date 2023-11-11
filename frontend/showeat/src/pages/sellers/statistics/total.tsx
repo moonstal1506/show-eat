82fd55bf3e485c9008ee2593aab8369ac3b474ac
ooyouj <ooyouj@naver.com>
2023-11-11 22:56:01 +0900

    Feat: Add total statistics page

--- frontend/showeat/src/pages/sellers/statistics/total.tsx
+++ frontend/showeat/src/pages/sellers/statistics/total.tsx
@@ -2,14 +2,54 @@
 import SellerLayout from "@layouts/SellerLayout";
 import withAuth from "@libs/withAuth";
 import { ReactNode } from "react";
-
+import Table from "@/components/common/table";
+import styled from "@emotion/styled";
 // ----------------------------------------------------------------------------------------------------
+const TotalContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 100px;
+`;
+
+const BusinessName = styled("div")`
+    color: #000;
+    font-family: Pretendard;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    padding-bottom: 100px;
+`;
+
+const TableWrapper = styled("div")`
+    width: 440px;
+    text-align: left;
+    padding-bottom: 20px;
+`;
 
 /* Total Statistics Page */
 function TotalStats() {
-    return <div>종합 매출</div>;
-}
+    const headers = [
+        "누적 매출액",
+        "누적 성공 펀딩수",
+        "누적 성공 펀딩 참여자수",
+        "누적 실패 펀딩수",
+    ];
 
+    const contents = ["123,456,000 원", "3 회", "323 명", "1 회"];
+    return (
+        <TotalContainer>
+            <BusinessName>야미화니 커피</BusinessName>
+            <TableWrapper>
+                <Table headerWidth="50%" headers={headers} contents={contents} />
+            </TableWrapper>
+        </TotalContainer>
+    );
+}
 // ----------------------------------------------------------------------------------------------------
 
 /* Middleware */
