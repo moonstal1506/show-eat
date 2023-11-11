69538a6b5aa8e880e698b91951ea4b183525e3ad
SSuSG <psg980331@naver.com>
2023-11-11 18:05:06 +0900

    Fix: Modify security config and funding

--- backend/src/main/java/com/ssafy/showeat/domain/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/showeat/domain/auth/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
                 .antMatchers("/api/auth/reissue").permitAll()
                 .antMatchers("/api/auth/login/**").permitAll()
+                .antMatchers(("/api/funding/**")).permitAll()
                 .anyRequest().authenticated();
 
         httpSecurity
