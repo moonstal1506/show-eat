aa3f9b6e88d35808c4d858daa152f532b9f79ea2
Narn99 <vkdldjtmflr@naver.com>
2023-11-11 23:09:37 +0900

    Feat: Add redeem page
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/pages/sellers/redeem.tsx
+++ frontend/showeat/src/pages/sellers/redeem.tsx
@@ -1,13 +1,165 @@
 /* Import */
-import MainLayout from "@/layouts/MainLayout";
 import withAuth from "@libs/withAuth";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
+import Webcam from "react-webcam";
+import jsQR from "jsqr";
+// import NextImage from "next/image";
+import styled from "@emotion/styled";
+import SingleLayout from "@/layouts/SingleLayout";
+import { TextButton } from "@/components/common/button";
+import { useRouter } from "next/router";
+import getQRCode from "@/apis/qr";
 
 // ----------------------------------------------------------------------------------------------------
 
+const RedeemContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    min-width: 800px;
+`;
+
+const TitleContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    padding-top: 1.4em;
+    padding-bottom: 0.6em;
+`;
+
+const TitleWrapper = styled("span")`
+    font-size: 30px;
+    font-weight: 700;
+
+    padding: 0.2em;
+`;
+
+const DescriptionWrapper = styled("span")`
+    font-size: 16px;
+    color: ${(props) => props.theme.colors.gray4};
+`;
+
+const WebcamWrapper = styled("div")`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 400px;
+
+    padding: 1em;
+`;
+
+const ButtonContainer = styled("div")`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    max-width: 450px;
+`;
+
 /* Seller Reading Redeem Page */
 function Redeem() {
-    return <div>쿠폰 읽기</div>;
+    const router = useRouter();
+    const webcamRef = useRef<Webcam>(null);
+    // const [screenshot, setScreenshot] = useState<string | null>(null);
+
+    const videoConstraints = {
+        // width: 480,
+        height: 360,
+        // facingMode: "user", // 'user'는 프론트 카메라, 'environment'는 후면 카메라
+    };
+
+    const readQRCode = (imageSrc: string) => {
+        const canvas = document.createElement("canvas");
+        const img = new Image();
+
+        img.src = imageSrc;
+
+        img.onload = function loadImage() {
+            if (img.width && img.height) {
+                canvas.width = img.width;
+                canvas.height = img.height;
+
+                const context = canvas.getContext("2d");
+                if (context) {
+                    context.drawImage(img, 0, 0, img.width, img.height);
+
+                    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+                    const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+                    if (code) {
+                        return code.data;
+                    }
+                }
+            }
+            return null;
+        };
+        return null;
+    };
+
+    const capture = () => {
+        const imageSrc = webcamRef.current?.getScreenshot() || "";
+        // setScreenshot(imageSrc);
+
+        // 여기에서 QR 코드를 읽어오는 로직을 추가합니다.
+        const qrCode = readQRCode(imageSrc);
+        if (qrCode) {
+            console.log("QR Code:", qrCode);
+            getQRCode(imageSrc).then((res) => {
+                console.log(res);
+            });
+        } else {
+            console.log("QR Code not found");
+        }
+    };
+
+    return (
+        <RedeemContainer>
+            <TitleContainer>
+                <TitleWrapper>발급한 쿠폰으로 결제하기</TitleWrapper>
+                <DescriptionWrapper>
+                    발급한 쿠폰 결제를 위해 웹캠에 QR 코드가 선명하게 나오도록 해주세요.
+                </DescriptionWrapper>
+            </TitleContainer>
+            <WebcamWrapper>
+                <Webcam
+                    style={{ borderRadius: "20px", maxHeight: "360px" }}
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={videoConstraints}
+                />
+            </WebcamWrapper>
+            <ButtonContainer>
+                <TextButton
+                    text="QR 코드 인식"
+                    width="200px"
+                    height="50px"
+                    fontSize={20}
+                    onClick={capture}
+                />
+                <TextButton
+                    text="취소"
+                    width="200px"
+                    height="50px"
+                    fill="negative"
+                    fontSize={20}
+                    onClick={() => router.back()}
+                />
+            </ButtonContainer>
+            {/* {screenshot && (
+                <div>
+                    <NextImage src={screenshot} alt="screenshot" width={200} height={200} />
+                </div>
+            )} */}
+        </RedeemContainer>
+    );
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -15,14 +167,14 @@ function Redeem() {
 /* Middleware */
 const RedeemWithAuth = withAuth({
     WrappedComponent: Redeem,
-    guardType: "USER_ONLY",
+    // guardType: "USER_ONLY",
 });
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Layout */
 RedeemWithAuth.getLayout = function getLayout(page: ReactNode) {
-    return <MainLayout>{page}</MainLayout>;
+    return <SingleLayout>{page}</SingleLayout>;
 };
 
 // ----------------------------------------------------------------------------------------------------
