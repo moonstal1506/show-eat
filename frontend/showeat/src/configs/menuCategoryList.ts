79e34d6f9ce6b2da451f485daad4b26cceab3b8b
Crassula1994 <gogun3535@gmail.com>
2023-11-13 15:54:23 +0900

    Refactor: Modfy menu button component
    Co-authored-by: Narn99 <https://github.com/Narn99>

--- frontend/showeat/src/configs/menuCategoryList.ts
+++ frontend/showeat/src/configs/menuCategoryList.ts
@@ -1,15 +1,44 @@
 /* Menu Category Config */
 const menuCategoryList = [
-    { id: "KOREAN", value: "한식" },
-    { id: "CHINESE", value: "중식" },
-    { id: "JAPANESE_SUSHI", value: "일식/회" },
-    { id: "WESTERN", value: "양식" },
-    { id: "CHICKEN_BURGER", value: "치킨/버거" },
-    { id: "ASIAN", value: "아시안" },
-    { id: "SNACKS_LATE_NIGHT", value: "분식/야식" },
+    {
+        id: "KOREAN",
+        value: "한식",
+        imageUrl: "/assets/images/korean-menu-icon.png",
+    },
+    {
+        id: "CHINESE",
+        value: "중식",
+        imageUrl: "/assets/images/chinese-menu-icon.png",
+    },
+    {
+        id: "JAPANESE_SUSHI",
+        value: "일식/회",
+        imageUrl: "/assets/images/japanese-menu-icon.png",
+    },
+    {
+        id: "WESTERN",
+        value: "양식",
+        imageUrl: "/assets/images/western-menu-icon.png",
+    },
+    {
+        id: "CHICKEN_BURGER",
+        value: "치킨/버거",
+        imageUrl: "/assets/images/chicken-menu-icon.png",
+    },
+    {
+        id: "ASIAN",
+        value: "아시안",
+        imageUrl: "/assets/images/asian-menu-icon.png",
+    },
+    {
+        id: "SNACKS_LATE_NIGHT",
+        value: "분식/야식",
+        imageUrl: "/assets/images/snack-menu-icon.png",
+    },
     {
         id: "CAFE_DESSERT",
         value: "카페/디저트",
+        imageUrl: "/assets/images/dessert-menu-icon.png",
     },
 ];
 
