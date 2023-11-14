1377c644db354f1bd219012b76112cb8e52c3ebb
sujeong <msj1506@naver.com>
2023-11-15 06:38:33 +0900

    Test: Remove daum post

--- frontend/showeat/src/pages/application/seller-info.tsx
+++ frontend/showeat/src/pages/application/seller-info.tsx
@@ -165,32 +165,32 @@ function SellerInfo({
         }
     };
 
-    useEffect(() => {
-        const loadDaumPostcode = () => {
-            const script = document.createElement("script");
-            script.src = "https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js";
-            // script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"; 로컬
-            script.async = true;
-            document.body.appendChild(script);
-        };
+    // useEffect(() => {
+    //     const loadDaumPostcode = () => {
+    //         const script = document.createElement("script");
+    //         script.src = "https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js";
+    //         // script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"; 로컬
+    //         script.async = true;
+    //         document.body.appendChild(script);
+    //     };
 
-        if (!window.daum) {
-            loadDaumPostcode();
-        }
-    }, []);
+    //     if (!window.daum) {
+    //         loadDaumPostcode();
+    //     }
+    // }, []);
 
-    const onClickAddress = () => {
-        if (window.daum && window.daum.Postcode) {
-            new window.daum.Postcode({
-                oncomplete(data: IAddr) {
-                    setBusinessAddress(data.address);
-                    setZonecode(data.zonecode);
-                    onBusinessAddressChange(data.address);
-                    onZonecodeChange(data.zonecode);
-                },
-            }).open();
-        }
-    };
+    // const onClickAddress = () => {
+    //     if (window.daum && window.daum.Postcode) {
+    //         new window.daum.Postcode({
+    //             oncomplete(data: IAddr) {
+    //                 setBusinessAddress(data.address);
+    //                 setZonecode(data.zonecode);
+    //                 onBusinessAddressChange(data.address);
+    //                 onZonecodeChange(data.zonecode);
+    //             },
+    //         }).open();
+    //     }
+    // };
 
     return (
         <InputContainer>
@@ -239,7 +239,7 @@ function SellerInfo({
                     onChange={handleZonecodeChange}
                 />
                 <ButtonWrapper>
-                    <TextButton width="100px" text="검색" onClick={onClickAddress} />
+                    <TextButton width="100px" text="검색" />
                 </ButtonWrapper>
             </InputBox>
             <InputBox>
