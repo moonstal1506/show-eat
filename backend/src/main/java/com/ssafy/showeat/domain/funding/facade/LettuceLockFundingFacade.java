0d11589b429b54f69bdfa38b8297b9fd1b8e09b9
sujeong <msj1506@naver.com>
2023-11-15 10:47:38 +0900

    Fix: Add IOException

--- backend/src/main/java/com/ssafy/showeat/domain/funding/facade/LettuceLockFundingFacade.java
+++ backend/src/main/java/com/ssafy/showeat/domain/funding/facade/LettuceLockFundingFacade.java
@@ -1,7 +1,10 @@
 package com.ssafy.showeat.domain.funding.facade;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 
+import com.google.zxing.WriterException;
 import com.ssafy.showeat.domain.funding.repository.RedisLockFundingRepository;
 import com.ssafy.showeat.domain.funding.service.FundingService;
 import com.ssafy.showeat.domain.user.entity.User;
@@ -15,14 +18,13 @@ public class LettuceLockFundingFacade {
 	private final FundingService fundingService;
 	private final RedisLockFundingRepository redisLockFundingRepository;
 
-	public void applyFunding(Long fundingId, User user) throws InterruptedException {
+	public void applyFunding(Long fundingId, User user) throws InterruptedException, IOException, WriterException {
 		while (!redisLockFundingRepository.lock(fundingId.toString())) {
 			Thread.sleep(100);
 		}
 
 		try {
 			fundingService.applyFunding(fundingId, user);
-
 		} finally {
 			redisLockFundingRepository.unlock(fundingId.toString());
 		}
