4a72cdd3c39ff385b7d04d2657f80a879bd50283
Narn99 <vkdldjtmflr@naver.com>
2023-11-17 15:04:30 +0900

    Chore: Modify head title in coupon page
    Co-authored-by: Crassula1994 <https://github.com/Crassula1994>

--- frontend/showeat/src/pages/buyers/coupons/[...tabNames].tsx
+++ frontend/showeat/src/pages/buyers/coupons/[...tabNames].tsx
@@ -214,9 +214,7 @@ function CouponsTab(props: CouponsTabProps) {
     return (
         <>
             <Head>
-                <title>
-                    내 {buyersCouponsTabMenu.find((tab) => tab.id === tabName)?.labelText} 쿠폰
-                </title>
+                <title>내 쿠폰</title>
                 <meta name="description" content="바이어님께서 보유하신 쿠폰 목록입니다." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
